7821f55583ce143be4011119d9dfdb5e5f2ea35b
Lucas Keller <148141760+ha-rcacheC0@users.noreply.github.com>
2024-09-05 16:38:17 -0500

    modified app

--- app.js
+++ app.js
@@ -1,70 +1,54 @@
 const rs = require("readline-sync");
 
-// Ask the user, "What operation would you like to perform?"
-// Then the user enters one of these options: "/" "*" "-" "+"
-
-const calculator = () => {
+const operationFunction = () => {
   let operation;
-  let breaker = false;
-
   do {
-    operation = rs.question(`What operation would you like to perform? `);
-
+    operation = rs.question("Please enter a valid operation (+, -, *, /): ");
     if (
       operation !== "+" &&
       operation !== "-" &&
       operation !== "*" &&
       operation !== "/"
     ) {
-      console.log("This is not a valid input.");
-      breaker = true;
-    } else {
-      breaker = false;
+      console.log("Invalid operation, please try again.");
     }
-  } while (breaker);
+  } while (
+    operation !== "+" &&
+    operation !== "-" &&
+    operation !== "*" &&
+    operation !== "/"
+  );
+  return operation;
+};
 
-  let num1 = rs.questionInt(`Please enter your first number.`);
-  let num2;
+const numFunction = () => {
+  return rs.questionInt("Please enter a number: ");
+};
 
-  do {
-    num2 = rs.questionInt(`Your current operation is: ${num1} ${operation}
-Please enter your second number now for the result. `);
+const calculator = () => {
+  const operation = operationFunction();
+  const num1 = numFunction();
+  const num2 = numFunction();
+  let answer;
 
-    if (operation === "/" && num2 === 0) {
-      console.log(
-        `Error: Division by zero is not allowed. Please enter a valid number.`
-      );
-      breaker = true;
-    } else {
-      breaker = false;
-    }
-  } while (breaker);
+  switch (operation) {
+    case "+":
+      answer = num1 + num2;
+      break;
+    case "-":
+      answer = num1 - num2;
+      break;
+    case "*":
+      answer = num1 * num2;
+      break;
+    case "/":
+      answer = num1 / num2;
+      break;
+    default:
+      console.log("Invalid operation.");
+  }
 
-  const answer = () => {
-    let result = 0;
-
-    switch (operation) {
-      case "+":
-        result = num1 + num2;
-        break;
-
-      case "-":
-        result = num1 - num2;
-        break;
-      case "*":
-        result = num1 * num2;
-        break;
-      case "/":
-        result = num1 / num2;
-        break;
-      default:
-        console.log("Invalid operation.");
-    }
-
-    return result;
-  };
-
-  console.log(`The result is: ${answer()}`);
+  console.log(`The result is: ${answer}`);
 };
 
 calculator();
